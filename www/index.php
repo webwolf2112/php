ed7dff49620b6ba93ff8850e16c90aa59772a367
Vanessa Henson <vhenson@gogoair.com>
2017-12-06 16:46:19 -0700

    fixed the cache and the docker images should be able to build now

--- www/index.php
+++ www/index.php
@@ -1,2 +1,21 @@
-<h1>Sweet this is being served from a Docker container running PHP and my SQL</h1>
-<?php phpinfo(); ?>
+<?php
+  $servername = "127.0.0.1";
+  $username = "root";
+  $password = "docker";
+
+  phpinfo();
+
+  if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'We don\'t have mysqli!!!';
+} else {
+    echo 'Phew we have it!';
+}
+
+  $conn = new mysqli($servername, $username, $password);
+
+  if($conn->connect_err) {
+    echo "Connection was not successful";
+    die('Connection Failed: ' . $conn->connect_error);
+  }
+  echo "Connected Successfully";
+?>
