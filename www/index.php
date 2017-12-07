6a57e56ba9111f3c0872eaa7d6ecbad731121348
Vanessa Henson <vanessahenson@Vanessas-Air.hsd1.co.comcast.net>
2017-12-06 19:45:39 -0700

    added the database and got php pulling out the data

--- www/index.php
+++ www/index.php
@@ -1,21 +1,40 @@
 <?php
-  $servername = "127.0.0.1";
+  $servername = "db";
   $username = "root";
   $password = "docker";
+  $table = "returnPath";
 
-  phpinfo();
+$con = mysqli_connect($servername, $username, $password, $table);
 
-  if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
-    echo 'We don\'t have mysqli!!!';
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
 } else {
-    echo 'Phew we have it!';
+  $mysqli = new mysqli($servername, $username, $password, $table);
+  $getData = "SELECT * FROM emails";
+  //$result =  mysqli_query($con, $getData);
+  echo "Connected Successfully";
+
+  if ($result = $mysqli->query($getData)) {
+    printf("Select returned %d rows.\n", $result->num_rows, "\n");
+
+    echo "\n" . '<pre>' . var_dump($result) . '</pre>';
+
+    while($row = $result->fetch_assoc()) {
+
+        echo "<br>" . "To Email " . $row["to_email"]. " From Email " . $row["from_email"] . "  Date " . $row["date_email"] . " Subject " . $row["subject"];
+    }
+
+
+    /* free result set */
+    $result->close();
 }
 
-  $conn = new mysqli($servername, $username, $password);
 
-  if($conn->connect_err) {
-    echo "Connection was not successful";
-    die('Connection Failed: ' . $conn->connect_error);
-  }
-  echo "Connected Successfully";
+mysqli_close($con);
+}
+
+//SELECT * FROM emails WHERE from_email = 'chris@email.com';
+//SELECT * FROM emails;
 ?>
