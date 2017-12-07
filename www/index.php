f052e5002fb365f74c8a9518626ff246da66c21f
Vanessa Henson <vanessahenson@Vanessas-Air.hsd1.co.comcast.net>
2017-12-06 20:25:47 -0700

    have the htaccess added and some of the server requests

--- www/index.php
+++ www/index.php
@@ -1,39 +1,49 @@
 <?php
+  //database
   $servername = "db";
   $username = "root";
   $password = "docker";
   $table = "returnPath";
 
-$con = mysqli_connect($servername, $username, $password, $table);
+  //api server
+  $serverRequest = $_SERVER['REQUEST_METHOD'];
+  $url_elements = explode('/', $_SERVER['PATH_INFO']);
 
-// Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-} else {
-  $mysqli = new mysqli($servername, $username, $password, $table);
-  $getData = "SELECT * FROM emails";
-  //$result =  mysqli_query($con, $getData);
-  echo "Connected Successfully";
+  echo $serverRequest. "<br>";
+  echo $_SERVER['PATH_INFO'] . '<br><br>';
 
-  if ($result = $mysqli->query($getData)) {
-    printf("Select returned %d rows.\n", $result->num_rows, "\n");
+  $con = mysqli_connect($servername, $username, $password, $table);
 
-    echo "\n" . '<pre>' . var_dump($result) . '</pre>';
+    // Check connection
+    if (mysqli_connect_errno())
+    {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    } else {
 
-    while($row = $result->fetch_assoc()) {
+      //query the database for all of the rows
+      $mysqli = new mysqli($servername, $username, $password, $table);
+      $getData = "SELECT * FROM emails";
 
-        echo "<br>" . "To Email " . $row["to_email"]. " From Email " . $row["from_email"] . "  Date " . $row["date_email"] . " Subject " . $row["subject"];
+      //parse through the results
+      if ($result = $mysqli->query($getData)) {
+        printf("Select returned %d rows.\n", $result->num_rows, "\n");
+
+        //loop through each row to parse the data
+        while($row = $result->fetch_assoc()) {
+
+            echo "<br>" . "To Email " . $row["to_email"]. " From Email " . $row["from_email"] . "  Date " . $row["date_email"] . " Subject " . $row["subject"];
+        }
+        /* free result set */
+        $result->close();
     }
 
-
-    /* free result set */
-    $result->close();
-}
+    mysqli_close($con);
+  }
 
 
-mysqli_close($con);
-}
+
+
+  //deliver the endpoint
 
 //SELECT * FROM emails WHERE from_email = 'chris@email.com';
 //SELECT * FROM emails;
